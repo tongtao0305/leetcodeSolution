38600d373db42457d239ce5dc10955a50ad0b49f
童涛 <18373105@buaa.edu.cn>
2020-05-07 00:08:21 +0800

    P1 finished

---
+++ 1.两数之和.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    //使用了一遍哈希表，是已知的最高效的解决方法
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { i, map.get(complement) };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Slution!");
+    }
+
+    // 传统的暴力方法，此外还有两遍哈希表的方法
+    public int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (j == i) {
+                    continue;
+                }
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
+// @lc code=end
